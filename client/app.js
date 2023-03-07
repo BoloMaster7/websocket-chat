1dd5c2ec661ee07d6b2003465f76ca976132b6d3
Pawel Godula <bolomaster7@gmail.com>
2023-03-07 18:43:49 +0100

    fix render, add server

--- client/app.js
+++ client/app.js
@@ -3,10 +3,10 @@ const messagesSection = document.querySelector('#messages-section');
 const messagesList= document.querySelector('#messages-list');
 const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
-const messageContentInpu = document.querySelector('#message-content');
+const messageContentInput = document.querySelector('#message-content');
 
 
-let userName = '';
+let userName ='';
 
 const login = (e) => {
   e.preventDefault();
@@ -20,15 +20,32 @@ const login = (e) => {
 }
 };
 
+console.log('length', messageContentInput.value.length)
 const sendMessage = (e) => {
   e.preventDefault();
   if (messageContentInput.value.length > 0) {
+   
     addMessage(userName, messageContentInput.value);
     messageContentInput.value = "";
 } else {
     alert('Please enter a message');
 }
 }
-  
-loginForm.addEventListener('submit', (e) => login(e));
-addMessageForm.addEventListener('submit', (e) => sendMessage(e)); 
+
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener("submit", (e) => sendMessage(e));
